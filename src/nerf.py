24077d9a5fcbbd95ad834e500ae40891ee7780d3
liangkaizhang <kj2519602@gmail.com>
2022-02-12 11:12:49 -0800

    Use jit to accelerate sampling

--- src/nerf.py
+++ src/nerf.py
@@ -1,19 +1,38 @@
 from typing import List, Any, Callable
 import attr
+import functools
 
 from flax import linen as nn
 import jax.numpy as jnp
+from jax import jit, random, vmap
 
 from geometry import Rays
 from nerf_utils import sample_along_rays, positional_encoding
 
 
 @attr.s(frozen=True, auto_attribs=True)
-class NerfModuleConfig:
-    sample_near: float = 0.001  # The near clipping distance when sampling along rays.
-    sample_far: float = 1000.  # The far clipping distance when sampling along rays.
+class NetConfig:
+    near_clip: float = 0.001  # The near clipping distance when sampling along rays.
+    far_clip: float = 1000.  # The far clipping distance when sampling along rays.
     num_samples: int = 100  # Number of sampling points along rays.
-    is_random: bool = True
+    randomized: bool = True  # Whether random sample.
+
+class Network(nn.Module):
+    config: NetConfig
+
+    @nn.compact
+    def __call__(self, rng: jnp.ndarray, posi_encode, view_ecode):
+        pass
+
+
+@attr.s(frozen=True, auto_attribs=True)
+class NerfModuleConfig:
+    near_clip: float = 0.001  # The near clipping distance when sampling along rays.
+    far_clip: float = 1000.  # The far clipping distance when sampling along rays.
+    num_samples: int = 100  # Number of sampling points along rays.
+    randomized: bool = True  # Whether random sample.
+    use_views: bool = True
+    net_config: NetConfig = NetConfig()
 
 
 class NerfModule(nn.Module):
@@ -22,16 +41,27 @@ class NerfModule(nn.Module):
     @nn.compact
     def __call__(self, rng: jnp.ndarray, rays: Rays,
                  weights: jnp.ndarray=None, bins: jnp.ndarray=None):
-        points = sample_along_rays(rng, rays,
-                                   self.config.near,
-                                   self.config.far, 
-                                   self.config.num_samples,
-                                   self.config.is_random,
-                                   weights,
-                                   bins)
-        x = positional_encoding(points)
-        
-        return rgb, sigma, weight
+        key, rng = random.split(rng)
+        if weights is None:
+            num_rays = rays.origins.shape[0]
+            num_bins = self.config.num_samples
+            bins = jnp.linspace(self.config.near_clip, self.config.far_clip, num_bins)
+            bins = jnp.broadcast_to(bins, (num_rays, num_bins))
+            weights = jnp.ones_like(bins)
+
+        sampler_fn = jit(functools.partial(sample_along_rays,
+                                           num_samples=self.config.num_samples,
+                                           is_random=self.config.randomized))
+        points = sampler_fn(key, rays, bins, weights)
+        posi_encode = positional_encoding(points)
+        view_encode = None
+        if self.config.use_views:
+            views = jnp.broadcast_to(rays.directions[:, jnp.newaxis, :], points.shape)
+            view_encode = positional_encoding(views)
+
+        rgb_raw, sigma_raw = Network(rng, posi_encode, view_encode)
+
+        return rgb, sigma, new_bins, new_weights
 
 
 @attr.s(frozen=True, auto_attribs=True)
