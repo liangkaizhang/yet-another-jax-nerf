2438c7b72f84f332b2416d1b247ccd28cd6eba5f
liangkaizhang <kj2519602@gmail.com>
2022-02-12 18:33:21 -0800

    Add volumetric rendering and positional encoding functions.

--- src/nerf.py
+++ src/nerf.py
@@ -1,28 +1,79 @@
-from typing import List, Any, Callable
+from typing import Any, Callable
 import attr
 import functools
 
 from flax import linen as nn
 import jax.numpy as jnp
-from jax import jit, random, vmap
+from jax import jit, random
 
 from geometry import Rays
-from nerf_utils import sample_along_rays, positional_encoding
+from nerf_utils import sample_along_rays, positional_encoding, volumetric_rendering
 
 
 @attr.s(frozen=True, auto_attribs=True)
 class NetConfig:
-    near_clip: float = 0.001  # The near clipping distance when sampling along rays.
-    far_clip: float = 1000.  # The far clipping distance when sampling along rays.
-    num_samples: int = 100  # Number of sampling points along rays.
-    randomized: bool = True  # Whether random sample.
+  net_depth: int = 8  # The depth of the first part of MLP.
+  net_width: int = 256  # The width of the first part of MLP.
+  net_depth_condition: int = 1  # The depth of the second part of MLP.
+  net_width_condition: int = 128  # The width of the second part of MLP.
+  net_activation: Callable[Ellipsis, Any] = nn.relu  # The activation function.
+  skip_layer: int = 4  # The layer to add skip layers to.
+  num_rgb_channels: int = 3  # The number of RGB channels.
+  num_sigma_channels: int = 1  # The number of sigma channels.
 
-class Network(nn.Module):
-    config: NetConfig
+
+class Net(nn.Module):
+    """A simple MLP."""
+    net_config: NetConfig
 
     @nn.compact
-    def __call__(self, rng: jnp.ndarray, posi_encode, view_ecode):
-        pass
+    def __call__(self, x, condition=None):
+        """Evaluate the MLP.
+        Args:
+        x: jnp.ndarray(float32), [batch, num_samples, feature], points.
+        condition: jnp.ndarray(float32), [batch, feature], if not None, this
+            variable will be part of the input to the second part of the MLP
+            concatenated with the output vector of the first part of the MLP. If
+            None, only the first part of the MLP will be used with input x. In the
+            original paper, this variable is the view direction.
+        Returns:
+        raw_rgb: jnp.ndarray(float32), with a shape of
+            [batch, num_samples, num_rgb_channels].
+        raw_sigma: jnp.ndarray(float32), with a shape of
+            [batch, num_samples, num_sigma_channels].
+        """
+        feature_dim = x.shape[-1]
+        num_samples = x.shape[1]
+        x = x.reshape([-1, feature_dim])
+        dense_layer = functools.partial(
+            nn.Dense, kernel_init=nn.initializers.glorot_uniform())
+        inputs = x
+        for i in range(self.config.net_depth):
+            x = dense_layer(self.config.net_width)(x)
+            x = self.config.net_activation(x)
+            if i % self.config.skip_layer == 0 and i > 0:
+                x = jnp.concatenate([x, inputs], axis=-1)
+            raw_sigma = dense_layer(self.config.num_sigma_channels)(x).reshape(
+                [-1, num_samples, self.config.num_sigma_channels])
+
+        if condition is not None:
+            # Output of the first part of MLP.
+            bottleneck = dense_layer(self.config.net_width)(x)
+            # Broadcast condition from [batch, feature] to
+            # [batch, num_samples, feature] since all the samples along the same ray
+            # have the same viewdir.
+            condition = jnp.tile(condition[:, None, :], (1, num_samples, 1))
+            # Collapse the [batch, num_samples, feature] tensor to
+            # [batch * num_samples, feature] so that it can be fed into nn.Dense.
+            condition = condition.reshape([-1, condition.shape[-1]])
+            x = jnp.concatenate([bottleneck, condition], axis=-1)
+            # Here use 1 extra layer to align with the original nerf model.
+            for i in range(self.config.net_depth_condition):
+                x = dense_layer(self.config.net_width_condition)(x)
+                x = self.config.net_activation(x)
+        raw_rgb = dense_layer(self.config.num_rgb_channels)(x).reshape(
+            [-1, num_samples, self.config.num_rgb_channels])
+        return raw_rgb, raw_sigma
 
 
 @attr.s(frozen=True, auto_attribs=True)
@@ -32,6 +83,9 @@ class NerfModuleConfig:
     num_samples: int = 100  # Number of sampling points along rays.
     randomized: bool = True  # Whether random sample.
     use_views: bool = True
+    min_deg_point: int = 0   # The minimum degree of positional encoding for positions.
+    max_deg_point: int = 10  # The maximum degree of positional encoding for positions.
+    white_bkgd: bool = False
     net_config: NetConfig = NetConfig()
 
 
@@ -51,17 +105,21 @@ class NerfModule(nn.Module):
 
         sampler_fn = jit(functools.partial(sample_along_rays,
                                            num_samples=self.config.num_samples,
-                                           is_random=self.config.randomized))
-        points = sampler_fn(key, rays, bins, weights)
-        posi_encode = positional_encoding(points)
+                                           randomized=self.config.randomized))
+        points, points_z = sampler_fn(key, rays, bins, weights)
+        posi_encode = positional_encoding(posi_encode, self.config.min_deg_point,
+                                          self.config.max_deg_point)
         view_encode = None
         if self.config.use_views:
             views = jnp.broadcast_to(rays.directions[:, jnp.newaxis, :], points.shape)
-            view_encode = positional_encoding(views)
-
-        rgb_raw, sigma_raw = Network(rng, posi_encode, view_encode)
-
-        return rgb, sigma, new_bins, new_weights
+            view_encode = positional_encoding(views, self.config.min_deg_point,
+                                              self.config.max_deg_point)
+        raw_rgb, raw_sigma = Net(self.config.net_config)(posi_encode, view_encode)
+        rgb, disp, acc, new_weights = volumetric_rendering(raw_rgb,
+                                                           raw_sigma, points_z,
+                                                           rays.directions,
+                                                           self.config.white_bkgd)
+        return rgb, disp, acc, points_z, new_weights
 
 
 @attr.s(frozen=True, auto_attribs=True)
@@ -74,25 +132,15 @@ class NerfModel(nn.Module):
     config: NerfModelConfig
 
     @nn.compact
-    def __call__(self, rng_0: jnp.ndarray, rng_1: jnp.ndarray, rays: Rays):
-        """Nerf Model.
-        Args:
-            rng_0: random number generator for coarse model sampling.
-            rng_1: random number generator for fine model sampling.
-            rays: input ColoredRays, a namedtuple of ray origins, directions, and colors.
-        Returns:
-            ret: list, [(rgb_coarse, disp_coarse, acc_coarse), (rgb, disp, acc)]
-        """
-        x = sample_along_rays(rays, self.config.near, self.config.far,
-                              self.config.num_samples,  self.config.random_sample)
-        
-        return x
+    def __call__(self, rng: jnp.ndarray, rays: Rays):
+        """Nerf Model"""
+        rng0, rng1 = random.split(rng)
+        coarse_nerf = NerfModule(self.config.coarse_module_config)
+        coarse_rgb, _, bins, weights = coarse_nerf(rng0, rays)
 
-    def _preprocess_fn(self, x):
-        return NotImplemented
-    
-    def _model_impl(self, x):
-        return 
+        fine_nerf = NerfModule(self.config.fine_module_config)
+        fine_rgb, _, _, _ = fine_nerf(rng1, rays, bins, weights)
+        return coarse_rgb, fine_rgb
 
 
 
--- src/nerf_utils.py
+++ src/nerf_utils.py
@@ -36,17 +36,40 @@ def sample_along_rays(rng: jnp.ndarray, rays: Rays, all_bins, all_weights, num_s
         z_samples = sample_pdf(rng, bins, weights, num_samples, randomized)
         direction = z_samples[..., jnp.newaxis] * direction[jnp.newaxis, :]
         points = origin[jnp.newaxis, :] + direction
-        return points
+        return points, z_samples
 
     num_rays = rays.origins.shape[0]
     rngs = random.split(rng, num_rays)
-    points = vmap(sample_helper)(rngs, rays.origins, rays.directions, all_bins, all_weights)
-    return points
+    points, z_values = vmap(sample_helper)(rngs, rays.origins, rays.directions, all_bins, all_weights)
+    return points, z_values
 
 
-def positional_encoding(points: jnp.ndarray):
-    return points
+def positional_encoding(x: jnp.ndarray, min_deg: int, max_deg: int):
+    if min_deg == max_deg:
+        return x
+    scales = jnp.array([2**i for i in range(min_deg, max_deg)])
+    xb = jnp.reshape((x[Ellipsis, None, :] * scales[:, None]),
+                        list(x.shape[:-1]) + [-1])
+    four_feat = jnp.sin(jnp.concatenate([xb, xb + 0.5 * jnp.pi], axis=-1))
+    return jnp.concatenate([x] + [four_feat], axis=-1)
 
 
-def volumetric_rendering(points_rgb, points_sigma):
-    return
+def volumetric_rendering(points_rgb, points_sigma, points_z, dirs, white_bkgd=True):
+    dists = points_z[..., 1:] - points_z[..., :-1]
+    dists = jnp.concatenate([dists, 1e10 * jnp.ones_like(dists[..., :1])], axis=-1)
+    dists = dists * jnp.linalg.norm(dirs, axis=-1)
+
+    dists_sigma = points_sigma * dists
+    alpha = 1.0 - jnp.exp(-dists_sigma)
+
+    transmit = jnp.exp(-jnp.cumsum(dists_sigma[..., :-1], axis=-1))
+    transmit = jnp.concatenate([jnp.ones_like(transmit[..., :1]), transmit], axis=-1)
+    weights = alpha * transmit
+
+    rgb = jnp.sum(weights[..., jnp.newaxis] * points_rgb, axis=-2)
+    depth = jnp.sum(weights * points_z, axis=-1)
+    acc = jnp.sum(weights, axis=-1)
+    disp = acc / (depth + EPS)
+    if white_bkgd:
+        rgb = rgb + (1. - acc[..., jnp.newaxis])
+    return rgb, disp, acc, weights
